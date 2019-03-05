f1a9984248bcdafad26771fcc495ad82afb9cd4d
lilatan <lilatan39@gmail.com>
2019-03-05 09:58:32 -0500

    ADDED NOTES FROM LAB ACTIVITY 1,2, AND PROJECT DEMO 1 AND STARTED FRONTEND MOCKUP - LILA

---
+++ src/FrontEnd.js
@@ -0,0 +1,11 @@
+//Using front-end web technologies (HTML/CSS/JavaScript) create a
+// mock front-end for your project as a prototype. This prototype does not have to have any
+// functionality or connect ot a server, but it must give someone unfamiliar with your project an
+// idea of what your team plans to build just by looking at your prototype. For example, you
+// might build the GUI for your app but none of the buttons do anything when clicked.
+
+//You must use web technologies for the mockup. You will not receive credit if the entire
+// mockup is a static image or drawing. The idea is that this mockup will become your actual
+// front-end once the buttons connect to JavaScript code and a server.
+
+var players = []
