b83090badff857fd5603ef8a17fd7f9179193c14
lilatan <lilatan39@gmail.com>
2019-03-29 15:46:06 -0400

    Added GUI File - LILA

--- src/view/TriviaGameGUI.scala
+++ src/view/TriviaGameGUI.scala
@@ -1,5 +1,30 @@
 package view
 
-object TriviaGameGUI extends JFXApp{
+import java.awt.Button
 
+import javafx.scene.Scene
+import javafx.scene.layout.VBox
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.control.TextField
+
+object TriviaGameGUI extends JFXApp{
+  val inputDisplay: TextField = new TextField{
+    style = "-fx-font: 18 ariel;"
+  }
+  val outputDisplay: TextField = new TextField{
+    style = "-fx-font: 18 ariel;"
+  }
+  val button: Button = new Button {
+  }
+  this.stage = new PrimaryStage {
+    title = "GUI Example"
+    scene = new Scene() {
+      content = List(
+        new VBox() {
+          children = List(inputDisplay, button, outputDisplay)
+        }
+      )
+    }
+  }
 }
